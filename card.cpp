905f0fe2dadddfbad25d68dfcad968f4858b3a68
zacharydiringer <147426803+zacharydiringer@users.noreply.github.com>
2025-10-05 16:00:13 -0400

    Created card class

---
+++ card.cpp
@@ -0,0 +1,46 @@
+// card.cpp file for the Flip Card Project
+// Kyle Murrah, Rachel Barume, Zachary Diringer
+// Initialization of the card class
+
+// Import statements
+#include "card.h"
+#include <iostream>
+using namespace std;
+
+// Constructor for the card class
+card::card(string suitInput, string valueInput)
+{
+	suit = suitInput;
+	value = valueInput;
+} // end constructor
+
+// Overloaded function to print the card
+ostream& operator<<(ostream& os, const card& resp)
+{
+	os << resp.value << " of " << resp.suit << "s";
+	return os;
+} // end overloaded function
+
+// Function to get the suit of the card
+string card::getSuit()
+{
+	return suit;
+} // end getSuit
+
+// Function to get the value of the card
+string card::getValue()
+{
+	return value;
+} // end getValue
+
+// Function to set the suit of the card
+void card::setSuit(string suitInput)
+{
+	suit = suitInput;
+} // end setSuit
+
+// Function to set the value of the card
+void card::setValue(string valueInput)
+{
+	value = valueInput;
+} // end setValue
---
+++ card.h
@@ -0,0 +1,36 @@
+#pragma once
+
+//card.h file for the Flip Card Project
+//Kyle Murrah, Rachel Barume, Zachary Diringer
+//Declaration of the card class
+
+class card;
+
+// Import statements
+#include <iostream>
+using namespace std;
+
+class card
+{
+private:
+	string suit;
+	string value;
+public:
+	// Constructor for the card class
+	card(string suitInput, string valueInput);
+
+	// Overloaded function to print the card
+	friend ostream& operator<<(ostream& os, const card& resp);
+
+	// Function to get the suit of the card
+	string getSuit();
+
+	// Function to get the value of the card
+	string getValue();
+
+	// Function to set the suit of the card
+	void setSuit(string suitInput);
+
+	// Function to set the value of the card
+	void setValue(string valueInput);
+}; // end class card
